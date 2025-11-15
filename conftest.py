b43785707577594ebe2028c0c37846e58f02ddb4
gvv2065 <vparadise@ya.ru>
2025-11-16 01:43:30 +0300

    Revert "allure"
    This reverts commit 02b0b2bfd77a0427e7c496c043d61fce654c1522.

--- conftest.py
+++ conftest.py
@@ -1,13 +1,37 @@
 import pytest
 import allure
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
 
-
-@pytest.fixture(scope="function")
-def driver():
-    options = Options()
-    # options.add_argument("--headless")
-    driver = webdriver.Firefox(options)
+@pytest.fixture(params=["firefox", "chrome"], scope="function")
+def driver(request):
+    """
+    Parametrized fixture that yields both Firefox and Chrome drivers.
+    Each test runs twice - once with Firefox, once with Chrome.
+    """
+    browser = request.param
+    
+    if browser == "firefox":
+        options = FirefoxOptions()
+        # options.add_argument("--headless")
+        driver = webdriver.Firefox(options=options)
+    else:  # chrome
+        options = ChromeOptions()
+        # options.add_argument("--headless")
+        driver = webdriver.Chrome(options=options)
+    
     yield driver
     driver.quit()
+    
+    
+# @pytest.fixture(scope="function")
+# def driver():
+#     # driver = webdriver.Chrome(ChromeDriverManager().install())
+#     options = ChromeOptions()
+    
+#     # options.add_argument("--headless")
+#     driver = webdriver.Chrome(options)
+#     yield driver
+#     driver.quit()
