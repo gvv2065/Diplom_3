1b5fe0e8ef46d8b148ae19ebb3f02f46f20f83f0
gvv2065 <vparadise@ya.ru>
2025-11-16 01:50:05 +0300

    remove comment

--- conftest.py
+++ conftest.py
@@ -1,9 +1,7 @@
 import pytest
-import allure
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(params=["firefox", "chrome"], scope="function")
 def driver(request):
@@ -15,23 +13,10 @@ def driver(request):
     
     if browser == "firefox":
         options = FirefoxOptions()
-        # options.add_argument("--headless")
         driver = webdriver.Firefox(options=options)
     else:  # chrome
         options = ChromeOptions()
-        # options.add_argument("--headless")
         driver = webdriver.Chrome(options=options)
     
     yield driver
     driver.quit()
-    
-    
-# @pytest.fixture(scope="function")
-# def driver():
-#     # driver = webdriver.Chrome(ChromeDriverManager().install())
-#     options = ChromeOptions()
-    
-#     # options.add_argument("--headless")
-#     driver = webdriver.Chrome(options)
-#     yield driver
-#     driver.quit()
